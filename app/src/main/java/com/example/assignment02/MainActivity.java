f771c265c28d86cd7c25d211d5b7fc587b7aa750
Ravindu <rtjayasooriya@gmail.com>
2022-12-11 09:37:12 +0530

    complete date & time display

--- app/src/main/java/com/example/assignment02/MainActivity.java
+++ app/src/main/java/com/example/assignment02/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.assignment02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView dateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dateView = findViewById(R.id.dateView);
+        Date currentDate = Calendar.getInstance().getTime();
+        dateView.setText(currentDate.toString());
     }
+
 }
